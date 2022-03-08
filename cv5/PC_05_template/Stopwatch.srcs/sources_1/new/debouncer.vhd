60e770fe46511b2f3c2af89db65b0862c79336a5
xsvobo1q <marsvobo1@seznam.cz>
2022-03-08 15:17:41 +0100

    stopwatch and debouncer

--- cv5/PC_05_template/Stopwatch.srcs/sources_1/new/debouncer.vhd
+++ cv5/PC_05_template/Stopwatch.srcs/sources_1/new/debouncer.vhd
@@ -33,7 +33,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 ENTITY debouncer IS
   GENERIC(
-    DEB_PERIOD  : POSITIVE := 3
+    DEB_PERIOD  : POSITIVE
   );
   PORT(
     clk     : IN STD_LOGIC;
@@ -44,18 +44,38 @@ ENTITY debouncer IS
 END debouncer;
 
 ARCHITECTURE Behavioral OF debouncer IS
-  SIGNAL deb_cnt : POSITIVE := 1;
+
+  SIGNAL deb_cnt  : POSITIVE := 1;
+  SIGNAL deb_cnt_en : BOOLEAN := FALSE;
+  
 BEGIN
   PROCESS (clk)
   BEGIN
-    IF rising_edge(clk) THEN
-      btn_deb <= '0';
-      IF (clk_en = '1' AND deb_cnt = DEB_PERIOD) THEN
-        btn_deb <= btn;
-        deb_cnt <= 1;
-      ELSE
-        deb_cnt <= deb_cnt + 1;
+    IF rising_edge(clk) AND clk_en = '1' THEN
+      
+      IF btn = '1' THEN 
+        deb_cnt_en <= TRUE;
       END IF;
+      
+      IF deb_cnt_en THEN
+        deb_cnt <= deb_cnt + 1;
+        btn_deb <= '1';
+      ELSE
+        deb_cnt <= 1;
+        btn_deb <= '0';
+      END IF;
+      
+      IF deb_cnt > DEB_PERIOD THEN
+        IF btn = '1' THEN
+          deb_cnt <= 1;
+          btn_deb <= '1';
+        ELSE
+          deb_cnt <= 1;
+          deb_cnt_en <= FALSE;
+          btn_deb <= '0';
+        END IF;
+      END IF;
+ 
     END IF;
   END PROCESS;
 
---
+++ cv5/PC_05_template/Stopwatch.srcs/sources_1/new/stopwatch.vhd
@@ -0,0 +1,69 @@
+-- Code your design here
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.NUMERIC_STD.ALL;
+
+ENTITY stopwatch IS
+  PORT(
+    clk            : IN STD_LOGIC;
+    cnt_reset      : IN STD_LOGIC;
+    cnt_enable     : IN STD_LOGIC;
+    ce_100Hz       : IN STD_LOGIC;
+    disp_enable    : IN STD_LOGIC;
+    cnt_0          : OUT STD_LOGIC_VECTOR (3 DOWNTO 0) := (OTHERS => '0');
+    cnt_1          : OUT STD_LOGIC_VECTOR (3 DOWNTO 0) := (OTHERS => '0');
+    cnt_2          : OUT STD_LOGIC_VECTOR (3 DOWNTO 0) := (OTHERS => '0');
+    cnt_3          : OUT STD_LOGIC_VECTOR (3 DOWNTO 0) := (OTHERS => '0')
+  );
+END stopwatch;
+
+ARCHITECTURE Behavioral OF stopwatch IS
+
+  SIGNAL cnt_0_sig  :  STD_LOGIC_VECTOR (3 DOWNTO 0) := (OTHERS => '0');
+  SIGNAL cnt_1_sig  :  STD_LOGIC_VECTOR (3 DOWNTO 0) := (OTHERS => '0');
+  SIGNAL cnt_2_sig  :  STD_LOGIC_VECTOR (3 DOWNTO 0) := (OTHERS => '0');
+  SIGNAL cnt_3_sig  :  STD_LOGIC_VECTOR (3 DOWNTO 0) := (OTHERS => '0');
+
+BEGIN
+
+  PROCESS (clk)
+  BEGIN
+    IF rising_edge(clk) AND ce_100Hz = '1' THEN
+      IF cnt_reset = '1' THEN
+        cnt_0_sig <= (OTHERS => '0');
+        cnt_1_sig <= (OTHERS => '0');
+        cnt_2_sig <= (OTHERS => '0');
+        cnt_3_sig <= (OTHERS => '0');
+      ELSE
+        IF cnt_enable = '1' THEN
+          cnt_0_sig <= STD_LOGIC_VECTOR(UNSIGNED(cnt_0_sig) + 1);
+        END IF;
+        
+        IF disp_enable = '1' THEN
+          cnt_0 <= cnt_0_sig;
+          cnt_1 <= cnt_1_sig;
+          cnt_2 <= cnt_2_sig;
+          cnt_3 <= cnt_3_sig;
+        END IF;
+        
+        IF cnt_0_sig = "1001" THEN
+          cnt_0_sig <= (OTHERS => '0');
+          cnt_1_sig <= STD_LOGIC_VECTOR(UNSIGNED(cnt_1_sig) + 1);
+        ELSIF cnt_1_sig = "1001" THEN
+          cnt_1_sig <= (OTHERS => '0');
+          cnt_2_sig <= STD_LOGIC_VECTOR(UNSIGNED(cnt_2_sig) + 1);
+        ELSIF cnt_2_sig = "1001" THEN
+          cnt_2_sig <= (OTHERS => '0');
+          cnt_3_sig <= STD_LOGIC_VECTOR(UNSIGNED(cnt_3_sig) + 1);
+        ELSIF cnt_3_sig = "0101" THEN
+          cnt_0_sig <= (OTHERS => '0');
+          cnt_1_sig <= (OTHERS => '0');
+          cnt_2_sig <= (OTHERS => '0');
+          cnt_3_sig <= (OTHERS => '0');
+        END IF;
+        
+      END IF;
+    END IF;
+  END PROCESS;
+
+END Behavioral;
